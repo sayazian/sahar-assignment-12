dbc1b162e73274e1e24c7b4f0210844f2aa9d12d
sayazian <sahar.ayazian@gmail.com>
2025-09-23 14:46:33 -0700

    instead of saving totals, I show queries that print out totals.

--- assignment_12.sql
+++ assignment_12.sql
@@ -13,14 +13,12 @@ customer_id int not null,
 first_name varchar(100),
 last_name varchar(100),
 phone_number varchar(12),
-expenditure decimal(10,2),
 primary key (customer_id)
 );
 create table orders (
 order_id int not null,
 order_time DateTime,
 customer_id int,
-total decimal(10,2),
 primary key (order_id),
 foreign key (customer_id) references customers (customer_id)
 );
@@ -35,7 +33,7 @@ foreign key (order_id) references orders (order_id),
 foreign key (pizza_id) references pizzas (pizza_id)
 );
 insert into pizzas (pizza_id, pizza_name, pizza_price)
-values 
+values
 (1,	'Pepperoni & Cheese', 7.99),
 (2,	'Vegetarian', 9.99),
 (3,	'Meat Lovers', 14.99),
@@ -61,38 +59,31 @@ values
 (7, 4, 2, 3),
 (8, 4, 4, 1);
 
-update order_pizza
-join pizzas on pizzas.pizza_id = order_pizza.pizza_id
-set order_pizza.amount = pizzas.pizza_price * order_pizza.quantity;
 
 select * from order_pizza;
 
-set sql_safe_updates = 0;
 
-update orders
-join (
-select order_id, sum(amount) as order_total
-from order_pizza
-group by order_id
-) t on orders.order_id = t.order_id
-set orders.total = t.order_total;
-
-select * from orders;
+select orders.order_id, orders.customer_id,
+sum(pizzas.pizza_price * order_pizza.quantity) as order_total
+from orders
+join order_pizza on orders.order_id = order_pizza.order_id
+join pizzas on pizzas.pizza_id = order_pizza.pizza_id
+group by orders.order_id, orders.customer_id;
 
 -- Q4
-update customers
-join (
-select customer_id, sum(total) as customer_total
-from orders
-group by customer_id
-) t on customers.customer_id = t.customer_id
-set customers.expenditure = t.customer_total;
-set sql_safe_updates = 1;
+select customers.customer_id,
+sum(pizzas.pizza_price * order_pizza.quantity) as customer_total
+from customers
+join orders on orders.customer_id = customers.customer_id
+join order_pizza on orders.order_id = order_pizza.order_id
+join pizzas on pizzas.pizza_id = order_pizza.pizza_id
+group by customers.customer_id;
 
-select * from customers;
 
 -- Q5
-select customer_id, order_time, sum(total) as customer_total
+select orders.customer_id, orders.order_time,
+sum(pizzas.pizza_price * order_pizza.quantity) as customer_total
 from orders
-group by customer_id, order_time
-order by customer_id, order_time;
+join order_pizza on orders.order_id = order_pizza.order_id
+join pizzas on pizzas.pizza_id = order_pizza.pizza_id
+group by orders.customer_id, orders.order_time;
